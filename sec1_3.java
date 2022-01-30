4fabe1c7112a767c3f0a430ac76c576050297904
mrpankajinamdar <inamdarpankaj13@gmail.com>
2022-01-30 17:40:22 +0530

    first commit

---
+++ sec1_3.java
@@ -0,0 +1,8 @@
+
+// 3. Write a Java program to print 'Hello' on screen and then print your name on a separate line. 
+class sec1_3{
+	public static void main(String arg[]){
+	System.out.println("Hello\nPankaj");
+	
+	}
+}
